47f00dee43e6b5db8e0091df0abe1b66d73b4ff2
Piotr Zalewa <piotr@zalewa.info>
2010-09-20 18:49:40 +0100

    JSON stringify MooTools library replaced to make it work with Bespin

--- flightdeck/media/js/lib/mootools-1.2.5-core-nc.js
+++ flightdeck/media/js/lib/mootools-1.2.5-core-nc.js
@@ -3597,6 +3597,7 @@ provides: JSON
 ...
 */
 
+/* THIS WASN'T WORKING 
 var JSON = new Hash(this.JSON && {
 	stringify: JSON.stringify,
 	parse: JSON.parse
@@ -3634,6 +3635,56 @@ var JSON = new Hash(this.JSON && {
 	}
 
 });
+*/
+
+
+if (!this.JSON) this.JSON = {};
+
+//<1.2compat>
+/*
+JSON = new Hash({
+	stringify: JSON.stringify,
+	parse: JSON.parse
+});
+*/
+//</1.2compat>
+
+(function(){
+	
+	var specialChars = {'\b': '\\b', '\t': '\\t', '\n': '\\n', '\f': '\\f', '\r': '\\r', '"' : '\\"', '\\': '\\\\'};
+	var replaceChars = function(chr){
+		return specialChars[chr] || '\\u00' + Math.floor(chr.charCodeAt() / 16).toString(16) + (chr.charCodeAt() % 16).toString(16);
+	};
+	
+	JSON.encode = function(obj){
+		switch ($type(obj)){
+			case 'string':
+				return '"' + obj.replace(/[\x00-\x1f\\"]/g, replaceChars) + '"';
+			case 'array':
+				return '[' + String(obj.map(JSON.encode).clean()) + ']';
+			case 'object': case 'hash':
+				var string = [];
+				Object.each(obj, function(value, key){
+					var json = JSON.encode(value);
+					if (json) string.push(JSON.encode(key) + ':' + json);
+				});
+				return '{' + string + '}';
+			case 'number': case 'boolean': return String(obj);
+			case 'null': return 'null';
+		}
+		return null;
+	};
+
+	JSON.decode = function(string, secure){
+		if ($type(string) != 'string' || !string.length) return null;
+		if (secure && !(/^[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]*$/).test(string.replace(/\\./g, '@').replace(/"[^"\\\n\r]*"/g, ''))) return null;
+		return eval('(' + string + ')');
+	};
+	
+	JSON.stringify = JSON.stringify || JSON.encode;
+	JSON.decode = JSON.decode || JSON.parse;
+	
+})();
 
 
 /*
