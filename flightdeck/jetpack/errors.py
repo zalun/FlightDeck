cb2062cdec052ece8145adfbf226bc8a68851813
Piotr Zalewa <piotr@zalewa.info>
2010-10-01 14:56:48 +0100

    pep8

--- flightdeck/jetpack/errors.py
+++ flightdeck/jetpack/errors.py
@@ -1,37 +1,55 @@
 from exceptions import Exception
 
+
 class SelfDependencyException(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return repr(self.value)
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
 
 class FilenameExistException(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return repr(self.value)
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
 
 class UpdateDeniedException(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return repr(self.value)
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
 
 class AddingAttachmentDenied(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return repr(self.value)
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
 
 class AddingModuleDenied(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return repr(self.value)
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
 
 class SingletonCopyException(Exception):
-	def __init__(self):
-		""
-	def __str__(self):
-		return repr("SDK can't be copied")
+
+    def __init__(self):
+        ""
+
+    def __str__(self):
+        return repr("SDK can't be copied")
--- flightdeck/jetpack/managers.py
+++ flightdeck/jetpack/managers.py
@@ -1,24 +1,24 @@
 from django.db import models
 
+
 class PackageManager(models.Manager):
 
-	def active(self):
-		return self.filter(active=True)
+    def active(self):
+        return self.filter(active=True)
 
-	def disabled(self):
-		return self.filter(active=False)
+    def disabled(self):
+        return self.filter(active=False)
 
-	def addons(self):
-		return self.active().filter(type="a")
+    def addons(self):
+        return self.active().filter(type="a")
 
-	def libraries(self, active_only=True):
-		return self.active().filter(type="l")
+    def libraries(self, active_only=True):
+        return self.active().filter(type="l")
 
 
 #TODO: Add Library and Addon managers and use them inside Package and views
 
 class PackageRevisionManager(models.Manager):
 
-	def filter_by_slug(self, slug):
-		 return self.select_related().filter(package__slug=slug)
-
+    def filter_by_slug(self, slug):
+        return self.select_related().filter(package__slug=slug)
