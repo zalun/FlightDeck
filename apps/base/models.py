6ce988a8f0755c188f15aa0fd844c7c03b841acc
Sean <sean.monstar@gmail.com>
2011-02-16 17:31:41 -0800

    moved default_setters to its own method

--- apps/base/models.py
+++ apps/base/models.py
@@ -7,16 +7,19 @@ class BaseModel(models.Model):
     
     def save(self, **kwargs):
         if not self.pk:
-            for attrName in dir(self):
-                if attrName.find('default_') != 0:
-                    continue
-                
-                attr = getattr(self, attrName)
-                if callable(attr):
-                    field = attrName[8:]
-                    orig = getattr(self, field)
-                    if orig is None or orig == '':
-                        attr()
+            self.run_default_setters()
         
         self.full_clean()
         return super(BaseModel, self).save(**kwargs)
+    
+    def run_default_setters(self):
+        for attrName in dir(self):
+            if attrName.find('default_') != 0:
+                continue
+            
+            attr = getattr(self, attrName)
+            if callable(attr):
+                field = attrName[8:]
+                orig = getattr(self, field)
+                if orig is None or orig == '':
+                    attr()
