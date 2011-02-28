ab26ef16dfa5a6fd490fa758dd0ae90877d2e9e1
Piotr Zalewa <piotr@zalewa.info>
2011-02-28 13:09:45 +0000

    download binary file without setting the utf

--- apps/xpi/tests/test_views.py
+++ apps/xpi/tests/test_views.py
@@ -1,12 +1,17 @@
 import simplejson
+import os
+import commonware
 
 from test_utils import TestCase
 from nose.tools import eq_
 from mock import patch
 
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 from base.templatetags.base_helpers import hashtag
+
+log = commonware.log.getLogger('f.test')
 
 
 class TestViews(TestCase):
@@ -16,6 +21,12 @@ class TestViews(TestCase):
         self.hashtag = hashtag()
         self.check_download_url = reverse('jp_check_download_xpi',
                 args=[self.hashtag])
+        self.xpi_path = os.path.join(
+            settings.XPI_TARGETDIR, '%s.xpi' % self.hashtag)
+
+    def tearDown(self):
+        if os.path.isfile(self.xpi_path):
+            os.remove(self.xpi_path)
 
     @patch('os.path.isfile')
     def test_package_check_download(self, isfile):
@@ -32,3 +43,21 @@ class TestViews(TestCase):
         eq_(r.status_code, 200)
         response = simplejson.loads(r.content)
         assert response['ready']
+
+    def test_downloading_xpi(self):
+        """Check if the right file is downloaded
+        """
+        mimetype = 'text/plain; charset=x-user-defined'
+        uri = reverse('jp_test_xpi', args=[self.hashtag])
+        # no file check
+        response = self.client.get(uri)
+        eq_(response.status_code, 200)
+        eq_(response.content, '')
+        # create a file and check
+        with open(self.xpi_path, 'w') as xpi:
+            xpi.write('test')
+        response = self.client.get(uri)
+        eq_(response.status_code, 200)
+        eq_(response.content, 'test')
+
+
