5b5211ec03d22737444b4881a74a6d45eec13245
Sean McArthur <sean.monstar@gmail.com>
2011-04-09 08:30:53 -0700

    sidebar filter of search results, results will use queryset-like object

--- apps/search/views.py
+++ apps/search/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from jetpack.models import Package
+
 def results(r):
     search_term = r.GET.get('q')
     results = []
     if search_term:
-        pass
+        results = Package.objects.filter(name__icontains=search_term)
+
 
     return render_to_response('results.html', {
         'results': results,
+        'q': search_term,
     }, context_instance=RequestContext(r))
