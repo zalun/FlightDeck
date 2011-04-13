b7069e34ab27cf12b2faab165d02735f6188653f
Sean McArthur <sean.monstar@gmail.com>
2011-04-13 16:35:04 -0700

    templates for search results

--- apps/search/views.py
+++ apps/search/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.db.models import Q
 
 from jetpack.models import Package
 
 def results(r):
-    search_term = r.GET.get('q')
-    results = []
+    search_term = r.GET.get('q', '')
+    addons = []
+    libraries = []
     if search_term:
-        results = Package.objects.filter(name__icontains=search_term)
+        results = Package.objects.filter(Q(name__icontains=search_term) | Q(description__icontains=search_term))
+        addons = results.filter(type='a')
+        libraries = results.filter(type='l')
 
 
     return render_to_response('results.html', {
-        'results': results,
+        'addons': addons,
+        'libraries': libraries,
         'q': search_term,
     }, context_instance=RequestContext(r))
