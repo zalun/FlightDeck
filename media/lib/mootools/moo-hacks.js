0211944042f48d4e707fa9d4e03940ee60606457
Sean McArthur <sean.monstar@gmail.com>
2011-05-05 18:45:55 -0700

    fixes Bug 638630 - upgrade MooTools to 1.3.2

--- media/lib/mootools/moo-hacks.js
+++ media/lib/mootools/moo-hacks.js
@@ -11,44 +11,6 @@ patches landing, should have these changes pasted in.
 /*
 ---
 
-name: Element.Event.Pseudos
-
-description: Adds the functionality to add pseudo events for Elements
-
-license: MIT-style license
-
-authors:
-  - Arian Stolwijk
-
-requires: [Core/Element.Event, Events.Pseudos]
-
-provides: [Element.Event.Pseudos]
-
-...
-*/
-(function(){
-
-var pseudos = {},
-	copyFromEvents = ['once', 'throttle', 'pause'],
-	count = copyFromEvents.length;
-
-while (count--) pseudos[copyFromEvents[count]] = Events.lookupPseudo(copyFromEvents[count]);
-
-Event.definePseudo = function(key, listener){
-	pseudos[key] = Type.isFunction(listener) ? {listener: listener} : listener;
-	Events.definePseudo(key, listener);
-	return this;
-};
-
-var proto = Element.prototype;
-[Element, Window, Document].invoke('implement', Events.Pseudos(pseudos, proto.addEvent, proto.removeEvent));
-
-})();
-
-
-/*
----
-
 name: Element.Event.Pseudos.Keys
 
 description: Adds functionality fire events if certain keycombinations are pressed
@@ -69,57 +31,60 @@ provides: [Element.Event.Pseudos.Keys]
 
 var keysStoreKey = '$moo:keys-pressed',
 	keysKeyupStoreKey = '$moo:keys-keyup',
+	keysParsed = {},
+	parseKeys = function(text){
+		return text.replace(/ctrl/g, 'control').split('|').map(function(key) {
+			var arr = [];
+			arr.append(key.replace('++', function(){
+				arr.push('+'); // shift++ and shift+++a
+				return '';
+			}).split('+'));
+			return arr;
+		});
+	},
 	store = function(key, val){
 		this.store ? this.store(key, val) : this[key] = val;
 		return this;
 	},
 	retrieve = function(key, def){
 		return this.retrieve ? this.retrieve(key, def) : (this[key] || def);
+	},
+	keysPseudo = function(split, fn, args){
+		var event = args[0],
+			unparsedValue = split.value,
+			keyCombos = keysParsed[unparsedValue] || (keysParsed[unparsedValue] = parseKeys(unparsedValue)),
+			pressed = retrieve.call(this, keysStoreKey, []);
+	
+		pressed.include(event.key);
+	
+		if (keyCombos.some(function(combo){
+			return combo.every(function(key){
+				return pressed.contains(key);
+			});
+		})) fn.apply(this, args);
+	
+		store.call(this, keysStoreKey, pressed);
+	
+		if (!retrieve.call(this, keysKeyupStoreKey)){
+			var keyup = function(event){
+				(function(){
+					pressed = retrieve.call(this, keysStoreKey, []).erase(event.key);
+					store.call(this, keysStoreKey, pressed);
+				}).delay(0, this); // Fix for IE
+			};
+			store.call(this, keysKeyupStoreKey, keyup).addEvent('keyup', keyup);
+		}
+	
 	};
 
-Event.definePseudo('keys', function(split, fn, args){
-	var event = args[0],
-		keyCombos = split.value.split('|'),
-		pressed = retrieve.call(this, keysStoreKey, []);
-
-	keyCombos = keyCombos.map(function(key) {
-		var arr = [];
-		arr.append(key.replace(/ctrl/g, 'control').replace('++', function(){
-			arr.push('+'); // shift++ and shift+++a
-			return '';
-		}).split('+'));
-		return arr;
-	});
-
-	pressed.include(event.key);
-
-	if (keyCombos.some(function(combo){
-		return combo.every(function(key){
-			return pressed.contains(key);
-		});
-	})) {
-		fn.apply(this, args);
-		pressed = [];
-	}
-
-	store.call(this, keysStoreKey, pressed);
-
-	if (!retrieve.call(this, keysKeyupStoreKey)){
-		var keyup = function(event){
-			(function(){
-				pressed = retrieve.call(this, keysStoreKey, []).erase(event.key);
-				store.call(this, keysStoreKey, pressed);
-			}).delay(0, this); // Fix for IE
-		};
-		store.call(this, keysKeyupStoreKey, keyup).addEvent('keyup', keyup);
-	}
-
-});
+Event.definePseudo('keys', keysPseudo);
+Events.definePseudo('keys', keysPseudo);
 
 Object.append(Event.Keys, {
 	'shift': 16,
 	'control': 17,
 	'alt': 18,
+	'meta': (Browser.Platform.mac && Browser.firefox) ? 224 :  91,
 	'capslock': 20,
 	'pageup': 33,
 	'pagedown': 34,
@@ -140,227 +105,5 @@ Object.append(Event.Keys, {
 	"'": 222,
 	'+': 107
 });
-
-})();
-
-
-
-/*
----
-
-script: Keyboard.js
-
-name: Keyboard
-
-description: KeyboardEvents used to intercept events on a class for keyboard and format modifiers in a specific order so as to make alt+shift+c the same as shift+alt+c.
-
-license: MIT-style license
-
-authors:
-  - Perrin Westrich
-  - Aaron Newton
-  - Scott Kyle
-
-requires:
-  - Core/Events
-  - Core/Options
-  - Core/Element.Event
-  - Element.Event.Pseudos.Keys
-
-provides: [Keyboard]
-
-...
-*/
-(function(){
-
-	var Keyboard = this.Keyboard = new Class({
-
-		Extends: Events,
-
-		Implements: [Options],
-
-		options: {/*
-			onActivate: function(){},
-			onDeactivate: function(){},*/
-			defaultEventType: 'keydown',
-			active: false,
-			manager: null,
-			events: {},
-			nonParsedEvents: ['activate', 'deactivate', 'onactivate', 'ondeactivate', 'changed', 'onchanged']
-		},
-
-		initialize: function(options){
-			if (options && options.manager){
-				this._manager = options.manager;
-				delete options.manager;
-			}
-			this.setOptions(options);
-			this._setup();
-		},
-
-		addEvent: function(type, fn, internal){
-			return this.parent(Keyboard.parse(type, this.options.defaultEventType, this.options.nonParsedEvents), fn, internal);
-		},
-
-		removeEvent: function(type, fn){
-			return this.parent(Keyboard.parse(type, this.options.defaultEventType, this.options.nonParsedEvents), fn);
-		},
-
-		toggleActive: function(){
-			return this[this.isActive() ? 'deactivate' : 'activate']();
-		},
-
-		activate: function(instance){
-			if (instance){
-				if (instance.isActive()) return this;
-				//if we're stealing focus, store the last keyboard to have it so the relinquish command works
-				if (this._activeKB && instance != this._activeKB){
-					this.previous = this._activeKB;
-					this.previous.fireEvent('deactivate');
-				}
-				//if we're enabling a child, assign it so that events are now passed to it
-				this._activeKB = instance.fireEvent('activate');
-				Keyboard.manager.fireEvent('changed');
-			} else if (this._manager){
-				//else we're enabling ourselves, we must ask our parent to do it for us
-				this._manager.activate(this);
-			}
-			return this;
-		},
-
-		isActive: function(){
-			return this._manager ? (this._manager._activeKB == this) : (Keyboard.manager == this);
-		},
-
-		deactivate: function(instance){
-			if (instance){
-				if (instance === this._activeKB){
-					this._activeKB = null;
-					instance.fireEvent('deactivate');
-					Keyboard.manager.fireEvent('changed');
-				}
-			} else if (this._manager){
-				this._manager.deactivate(this);
-			}
-			return this;
-		},
-
-		relinquish: function(){
-			if (this.isActive() && this._manager && this._manager.previous) this._manager.activate(this._manager.previous);
-			else this.deactivate();
-			return this;
-		},
-
-		//management logic
-		manage: function(instance){
-			if (instance._manager) instance._manager.drop(instance);
-			this._instances.push(instance);
-			instance._manager = this;
-			if (!this._activeKB) this.activate(instance);
-			return this;
-		},
-
-		drop: function(instance){
-			instance.relinquish();
-			this._instances.erase(instance);
-			if (this._activeKB == instance){
-				if (this.previous && this._instances.contains(this.previous)) this.activate(this.previous);
-				else this._activeKB = this._instances[0];
-			}
-			return this;
-		},
-
-		trace: function(){
-			Keyboard.trace(this);
-		},
-
-		each: function(fn){
-			Keyboard.each(this, fn);
-		},
-
-		/*
-			PRIVATE METHODS
-		*/
-
-		_instances: [],
-
-		_disable: function(instance){
-			if (this._activeKB == instance) this._activeKB = null;
-		},
-
-		_setup: function(){
-			this.addEvents(this.options.events);
-			//if this is the root manager, nothing manages it
-			if (Keyboard.manager && !this._manager) Keyboard.manager.manage(this);
-			if (this.options.active) this.activate();
-			else this.relinquish();
-		},
-
-		_handle: function(event, type){
-			//Keyboard.stop(event) prevents key propagation
-			if (event.preventKeyboardPropagation) return;
-
-			var bubbles = !!this._manager;
-			if (bubbles && this._activeKB){
-				this._activeKB._handle(event, type);
-				if (event.preventKeyboardPropagation) return;
-			}
-			this.fireEvent(type, event);
-
-			if (!bubbles && this._activeKB) this._activeKB._handle(event, type);
-		}
-
-	});
-
-	var parsed = {};
-	var modifiers = ['shift', 'control', 'alt', 'meta'];
-	var regex = /^(?:shift|control|ctrl|alt|meta)$/;
-
-	Keyboard.parse = function(type, eventType, ignore){
-		if (ignore && ignore.contains(type.toLowerCase())) return type;
-
-		var keys = type.toLowerCase().replace(/^(keyup|keydown):?/, function($0, $1){
-			eventType = $1;
-			return '';
-		}).replace('ctrl', 'control');
-
-		
-
-		return eventType + (keys && ':keys(' + keys + ')');
-	};
-
-	Keyboard.each = function(keyboard, fn){
-		var current = keyboard || Keyboard.manager;
-		while (current){
-			fn.run(current);
-			current = current._activeKB;
-		}
-	};
-
-	Keyboard.stop = function(event){
-		event.preventKeyboardPropagation = true;
-	};
-
-	Keyboard.manager = new Keyboard({
-		active: true
-	});
-
-	Keyboard.trace = function(keyboard){
-		keyboard = keyboard || Keyboard.manager;
-		var hasConsole = window.console && console.log;
-		if (hasConsole) console.log('the following items have focus: ');
-		Keyboard.each(keyboard, function(current){
-			if (hasConsole) console.log(document.id(current.widget) || current.wiget || current);
-		});
-	};
-
-	var handler = function(event){
-		Keyboard.manager._handle(event, event.type);
-	};
-
-	document.addEvents({
-		'keyup': handler,
-		'keydown': handler
-	});
 
 })();
