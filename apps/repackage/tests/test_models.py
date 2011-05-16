a98f8a5211ea93910365310217eb683b4013f0be
Piotr Zalewa <piotr@zalewa.info>
2011-05-16 18:57:33 +0100

    testing forcing the new version

--- apps/repackage/tests/test_models.py
+++ apps/repackage/tests/test_models.py
@@ -23,10 +23,7 @@ class RepackageTest(TestCase):
         self.hashtag = hashtag()
         settings.XPI_AMO_PREFIX = "file://%s" % os.path.join(
                 settings.ROOT, 'apps/xpi/tests/sample_addons/')
-
-    # mock self.sdk.get_source_dir()
-    def test_repackage(self):
-        sample_addons = [
+        self.sample_addons = [
                 # 1.0b1 and 1.0b2 are not working as filename has to be
                 # the same as manifest['name']
                 # No such file or directory:
@@ -34,10 +31,26 @@ class RepackageTest(TestCase):
                 # "sample_add-on-1.0b1","sample_add-on-1.0b2",
                 "sample_add-on-1.0b3",
                 "sample_add-on-1.0b4" ]
-        sdk_source_dir = os.path.join(settings.ROOT, 'lib/addon-sdk-1.0b5')
-        for sample in sample_addons:
-            hashtag = self.hashtag
+        self.sdk_source_dir = os.path.join(
+                settings.ROOT, 'lib/addon-sdk-1.0b5')
+
+    def tearDown(self):
+        target_xpi = os.path.join(
+                settings.XPI_TARGETDIR, self.hashtag, '.xpi')
+        if os.path.isfile(target_xpi):
+            os.remove(target_xpi)
+
+    # mock self.sdk.get_source_dir()
+    def test_repackage(self):
+        for sample in self.sample_addons:
             rep = Repackage()
             rep.download(123, sample)
-            response = rep.rebuild(sdk_source_dir, hashtag)
+            response = rep.rebuild(self.sdk_source_dir, self.hashtag)
             assert not response
+
+    def test_forcing_version(self):
+        for sample in self.sample_addons:
+            rep = Repackage()
+            rep.download(123, sample)
+            rep.get_manifest('force.version')
+        eq_(rep.manifest['version'], 'force.version')
