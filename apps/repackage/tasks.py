8c1cb60ffe473b80bcb9bd8d218af39ed79d7de9
Piotr Zalewa <piotr@zalewa.info>
2011-05-27 16:06:25 +0100

    using kwargs in wrapper to provide more elegant solution

--- apps/repackage/tasks.py
+++ apps/repackage/tasks.py
@@ -16,31 +16,28 @@ from repackage.models import Repackage
 log = commonware.log.getLogger('f.repackage.tasks')
 
 @task(rate_limit='5/m')
-def low_download_and_rebuild(location, sdk_source_dir, hashtag,
-        package_overrides={}, filename=None, pingback=None, post=None):
+def low_download_and_rebuild(*args, **kwargs):
     """A wrapper for :meth:`download_and_rebuild` needed to create
     different route in celery for low priority rebuilds
     https://bugzilla.mozilla.org/show_bug.cgi?id=656978
     """
     log.info("Starting low priority package rebuild...")
-    return download_and_rebuild(location, sdk_source_dir, hashtag,
-        package_overrides, filename, pingback, post)
+    return download_and_rebuild(*args, **kwargs)
 
 
 @task(rate_limit='30/m')
-def high_download_and_rebuild(location, sdk_source_dir, hashtag,
-        package_overrides={}, filename=None, pingback=None, post=None):
+def high_download_and_rebuild(*args, **kwargs):
     """A wrapper for :meth:`download_and_rebuild` needed to create
     different route in celery for high priority rebuilds
     https://bugzilla.mozilla.org/show_bug.cgi?id=656978
     """
     log.info("Starting high priority package rebuild...")
-    return download_and_rebuild(location, sdk_source_dir, hashtag,
-        package_overrides, filename, pingback, post)
+    return download_and_rebuild(*args, **kwargs)
 
 
 def download_and_rebuild(location, sdk_source_dir, hashtag,
-        package_overrides={}, filename=None, pingback=None, post=None):
+        package_overrides={}, filename=None, pingback=None, post=None,
+        **kwargs):
     """creates a Repackage instance, downloads xpi and rebuilds it
 
     :params:
@@ -51,6 +48,7 @@ def download_and_rebuild(location, sdk_source_dir, hashtag,
         * filename (String) desired filename for the downloaded ``XPI``
         * pingback (String) URL to pass the result
         * post (String) urlified ``request.POST``
+        * kwargs is just collecting the task decorator overhead
 
     :returns: (list) ``cfx xpi`` response where ``[0]`` is ``stdout`` and
               ``[1]`` ``stderr``
