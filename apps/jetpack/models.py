27a65f731cd77c32da7a5261038c1bb3d15a1e70
Sean McArthur <sean.monstar@gmail.com>
2011-06-08 17:05:00 -0700

    fix failing xpi build test

--- apps/jetpack/models.py
+++ apps/jetpack/models.py
@@ -785,15 +785,15 @@ class PackageRevision(BaseModel):
             # save as new version
             self.save()
         return self.dependencies.add(dep)
-    
+
     def compare_dependency_conflicts(self, dep, as_upgrade=False):
         """
         check if adding a dependency will cause a naming conflict as per the
         SDK 1.0. raises a DependencyException.
-        
+
         :param: as_upgrade (Boolean) - if the passed `dep` should be compared
             for doing an upgade.
-        
+
         Example:
             self = 'A' -> 'B' -> 'C'
             dep = 'D' -> 'E' -> 'F' -> 'C'
@@ -804,32 +804,35 @@ class PackageRevision(BaseModel):
             :param: first_level (Boolean) - conflicts are more strict at the
                 first level
             """
-            
+
             is_self = existing == self
-            
+            is_dep = adding == dep
+
             # as an upgrade, don't compare the upgraded revision to the old
             # revision of the same package
             if as_upgrade and first_level and existing.package_id == dep.package_id:
                 return
-            
+
             # same id is fine, we will only include same id
             # once when building the xpi.
-            if ((first_level or existing.id != adding.id) and
-                existing.package.name == adding.package.name):
+            same_name = existing.package.name == adding.package.name
+            same_revision = existing.id == adding.id
+
+            if (same_name and (not same_revision or first_level)):
                 raise DependencyException(
                     'Your %s already depends on a library named "%s"' % (
                         self.package.get_type_name(),
                         adding.package.name))
             for lib in existing.dependencies.all():
-                check_conflicts_if_added(lib, adding, is_self)
-        
-        
+                check_conflicts_if_added(lib, adding, is_self and is_dep)
+
+
         def check_adding_all_dependencies(existing, adding):
             check_conflicts_if_added(existing, adding)
-            
+
             for lib in adding.dependencies.all():
                 check_adding_all_dependencies(existing, lib)
-        
+
         if not as_upgrade and self.dependencies.filter(
             package__name=dep.package.name).count():
             raise DependencyException(
