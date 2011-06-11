d34a5236cb47336a69655d93ee82f981b2466032
Sean McArthur <sean.monstar@gmail.com>
2011-06-10 18:15:28 -0700

    /repackage/sdk-versions/ should return a JSON list of all the SDK versions known to Builder

--- apps/repackage/views.py
+++ apps/repackage/views.py
@@ -191,3 +191,11 @@ def rebuild(request):
 
     return HttpResponse(simplejson.dumps(response),
             mimetype='application/json')
+
+
+def sdk_versions(r):
+    versions = SDK.objects.all()
+    response = [sdk.version for sdk in versions]
+    
+    return HttpResponse(simplejson.dumps(response),
+            mimetype='application/json')
