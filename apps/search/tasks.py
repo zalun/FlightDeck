a26c0a5ec79c67a110c5d7308140b0a159cafe16
Sean McArthur <sean.monstar@gmail.com>
2011-06-17 22:07:06 -0700

    log the start and successful end of index_all as well

--- apps/search/tasks.py
+++ apps/search/tasks.py
@@ -2,10 +2,15 @@ from celeryutils import task
 
 from jetpack.models import Package
 from elasticutils import get_es
+import commonware.log
 
+log = commonware.log.getLogger('f.search.tasks')
 
 @task
 def index_all(pks, **kw):
+    ids_str = ','.join(map(str, pks))
+    log.debug('ES starting bulk action for packages: [%s]' % ids_str)
+    
     for package in Package.objects.filter(pk__in=pks):
         package.refresh_index(bulk=True)
     
@@ -15,4 +20,6 @@ def index_all(pks, **kw):
         raise
     except Exception, e:
         log.error('ES failed bulk action (%s), package ids: [%s]'
-                  % (e, ','.join(map(str, pks))))
+                  % (e, ids_str))
+    else:
+        log.debug('ES finished bulk action for packages: [%s]' % ids_str)
