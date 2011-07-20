37e43c1906b455520e498ae983f6e0d8f4e5e9ed
Sean McArthur <sean.monstar@gmail.com>
2011-07-20 10:11:41 -0700

    py migration must be in a run() method

--- migrations/011-ensure_library_main_module.py
+++ migrations/011-ensure_library_main_module.py
@@ -2,19 +2,20 @@ from jetpack.models import PackageRevision
 
 LIB_MODULE_MAIN = 'index'
 
-libs = PackageRevision.objects.filter(package__type='l', module_main='main')
-        .select_related('package', 'modules')
+def run(*args, **kwargs):
+    libs = PackageRevision.objects.filter(package__type='l', module_main='main')
+            .select_related('package', 'modules')
 
-libs.update(module_main=LIB_MODULE_MAIN)
+    libs.update(module_main=LIB_MODULE_MAIN)
 
-main_per_package = {}
+    main_per_package = {}
 
-for revision in libs:
-    if revision.modules.filter(filename=LIB_MODULE_MAIN).count() == 0:
-        mod = main_per_package.get(revision.package_id)
-        if not mod:
-            mod = Module(filename=LIB_MODULE_MAIN, author=revision.author)
-            mod.save()
-            main_per_package[revision.package_id] = mod
+    for revision in libs:
+        if revision.modules.filter(filename=LIB_MODULE_MAIN).count() == 0:
+            mod = main_per_package.get(revision.package_id)
+            if not mod:
+                mod = Module(filename=LIB_MODULE_MAIN, author=revision.author)
+                mod.save()
+                main_per_package[revision.package_id] = mod
 
-        revision.modules.add(mod)
+            revision.modules.add(mod)
