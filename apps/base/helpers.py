efa336fc97b337a6e3adbfb3d92b3227c1389d90
Sean McArthur <sean.monstar@gmail.com>
2011-08-05 17:00:31 -0700

    added querystring templatetag
    * borrowing from zamboni, this lets us update the existing query string sanely

---
+++ apps/base/helpers.py
@@ -0,0 +1,32 @@
+import urllib
+import urlparse
+from django.utils.encoding import smart_str
+
+def querystring(q, **query):
+    """
+    Add paramaters to a query string. New params will be appended, duplicates will
+    be overwritten.
+    """
+    query_dict = dict(urlparse.parse_qsl(smart_str(q))) if q else {}
+
+    query_dict.update(**query)
+    query_string = urlencode([(k ,v) for k, v in query_dict.items()
+                             if v is not None])
+    return query_string
+
+def urlparams(url_, **query):
+    """
+    Update the parameters of a url and return it.
+    """
+    url = urlparse.urlparse(url_)
+    query_string = querystring(url.query)
+    new = urlparse.ParseResult(url.scheme, url.netloc, url.path, url.params,
+                               querty_string, url.fragment)
+    return new.geturl()
+
+def urlencode(items):
+    """A Unicode-safe URLencoder."""
+    try:
+        return urllib.urlencode(items)
+    except UnicodeEncodeError:
+        return urllib.urlencode([(k, smart_str(v)) for k, v in items])
