ede4f53650330ec680763ea4344dc31ee5d06319
Piotr Zalewa <piotr@zalewa.info>
2011-08-22 15:42:48 +0100

    Move upload validation to view to allow displaying an error

--- apps/amo/tasks.py
+++ apps/amo/tasks.py
@@ -23,27 +23,6 @@ def upload_to_amo(rev_pk, hashtag=None):
     if not hashtag:
         hashtag = get_random_string(10)
     revision = PackageRevision.objects.get(pk=rev_pk)
-    # check if there this Add-on was uploaded with the same version name
-    version = revision.get_version_name_only()
-    uploaded = PackageRevision.objects.filter(
-            package=revision.package).filter(
-            amo_version_name=version).exclude(
-            amo_status=None).exclude(
-            amo_status=STATUS_UPLOAD_FAILED).exclude(
-            amo_status=STATUS_UPLOAD_SCHEDULED)
-    if len(uploaded) > 0:
-        log.debug("This Add-on was already uploaded using version \"%s\"" % version)
-        log.debug(revision.amo_status)
-        raise SimpleException("This Add-on was already uploaded using version \"%s\"" % version)
-    try:
-        PackageRevision.objects.get(
-            package=revision.package, amo_version_name=version,
-            amo_status=STATUS_UPLOAD_SCHEDULED)
-    except:
-        pass
-    else:
-        log.debug("This Add-on is currently scheduled to upload")
-        raise SimpleException("This Add-on is currently scheduled to upload")
     # save status
     revision.amo_status = STATUS_UPLOAD_SCHEDULED
     revision.amo_version_name = revision.get_version_name_only()
