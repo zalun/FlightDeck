3baec0b0d1b49fedef2a97b696b98b4feac701f4
Sean McArthur <sean.monstar@gmail.com>
2011-09-21 16:08:04 -0500

    v0.9.12

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -88,4 +88,6 @@ def update_flightdeck(ctx):
 
     # Run management commands like this:
     # manage_cmd(ctx, 'cmd')
+    manage_cmd(ctx, 'cron setup_mapping')
+    manage_cmd(ctx, 'cron index_all')
 
