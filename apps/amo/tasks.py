4ec67ff4da54dd5280be28752f8f953cb005fc25
Piotr Zalewa <piotr@zalewa.info>
2011-09-26 22:43:25 +0100

    fix 500 in tasks

--- apps/amo/tasks.py
+++ apps/amo/tasks.py
@@ -6,7 +6,7 @@ from utils.exceptions import SimpleException
 from celery.decorators import task
 
 from amo.constants import *
-from jetpack.models import PackageRevision
+from jetpack.models import PackageRevision, STATUS_UPLOAD_SCHEDULED
 from utils.helpers import get_random_string
 from xpi import xpi_utils
 
@@ -24,11 +24,15 @@ def upload_to_amo(rev_pk, hashtag=None):
         hashtag = get_random_string(10)
     revision = PackageRevision.objects.get(pk=rev_pk)
     # save status
+    log.debug("[%s] upload - Saving status")
     revision.amo_status = STATUS_UPLOAD_SCHEDULED
     revision.amo_version_name = revision.get_version_name()
     super(PackageRevision, revision).save()
+    log.debug("[%s] upload - Building XPI")
     response = revision.build_xpi(
             hashtag=hashtag,
             tstart=tstart)
     # use created XPI
+    log.debug("[%s] upload - XPI built - uploading")
     revision.upload_to_amo(hashtag)
+    log.debug("[%s] upload finished")
