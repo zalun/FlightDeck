633c0e8c5815d8d4f9d2b590c88fcda49abfeaff
Arron Schaar MBP <aschaar@gmail.com>
2011-10-13 13:52:55 -0700

    added index to packagerevision table

--- migrations/019-refactor_package_activity.sql
+++ migrations/019-refactor_package_activity.sql
@@ -1,3 +1,6 @@
 ALTER TABLE jetpack_package DROP COLUMN year_of_activity;
 ALTER TABLE jetpack_package DROP COLUMN activity_updated_at;
 ALTER TABLE jetpack_package ADD COLUMN activity_rating decimal(4,3);
+
+-- this index is intended to speed up the package activity queries.
+CREATE INDEX package_id_created_at ON jetpack_packagerevision (package_id, created_at DESC);
