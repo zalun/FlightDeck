6aa9a88c63ab88b59a054b86487c85d72fc6f432
Arron Schaar MBP <aschaar@gmail.com>
2011-10-18 11:24:43 -0700

    updated deploy script to run setup_mapping

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -89,6 +89,7 @@ def update_flightdeck(ctx):
     # Run management commands like this:
     # manage_cmd(ctx, 'cmd')
 
-    manage_cmd(ctx, 'cron update_package_activity') 
+    manage_cmd(ctx, 'cron update_package_activity')
+    manage_cmd(ctx, 'cron setup_mapping') 
 
 
