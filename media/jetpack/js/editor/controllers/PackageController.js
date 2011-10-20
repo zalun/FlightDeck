3b7f5be91e3ce9d6a3e7d0e91e520f82c7a57b7d
Sean McArthur <sean.monstar@gmail.com>
2011-10-19 19:30:46 -0500

    added tests for all of previous Package and Package.View methods

--- media/jetpack/js/editor/controllers/PackageController.js
+++ media/jetpack/js/editor/controllers/PackageController.js
@@ -17,12 +17,26 @@ module.exports = new Class({
         modules: [],
         attachments: [],
         dependencies: [],
-        folders: []
+        folders: [],
+        readonly: false,
+        package_info_el: 'package-properties',
+        copy_el: 'package-copy',
+        test_el: 'try_in_browser',
+        download_el: 'download',
+        console_el: 'error-console',
+        save_el: 'package-save',
+        menu_el: 'UI_Editor_Menu',
+        
+
+        check_if_latest: true  // switch to false if displaying revisions
+
     },
     
     package: null,
 
     //junk
+    data: {},
+
     modules: {},
     attachments: {},
 
@@ -42,70 +56,85 @@ module.exports = new Class({
         }
 
         this.instantiate_modules();
-		this.instantiate_attachments();
-		this.instantiate_folders();
-		this.instantiate_dependencies();
-		// hook event to menu items
-		this.revision_list_btn = $('revisions_list')
+        this.instantiate_attachments();
+        this.instantiate_folders();
+        this.instantiate_dependencies();
+        // hook event to menu items
+        this.revision_list_btn = $('revisions_list')
         this.revision_list_btn.addEvent('click', function(e) {
             controller.showRevisionList();
         });
-		if (this.package.isAddon()) {
+        if (this.package.isAddon()) {
             this.boundTestAddon = this.testAddon.bind(this);
-			this.options.test_url = $(this.options.test_el).getElement('a').get('href');
-			$(this.options.test_el).addEvent('click', this.boundTestAddon);
-            if (!this.boundDownloadAddon) {
-                this.boundDownloadAddon = this.downloadAddon.bind(this);
-            }
-			this.options.download_url = $(this.options.download_el).getElement('a').get('href');
-			$(this.options.download_el).addEvent('click', this.boundDownloadAddon);
-		}
-		this.copy_el = $(this.options.copy_el)
-		if (this.copy_el) {
-			this.copy_el.addEvent('click', this.copyPackage.bind(this));
-		}
+            this.test_el = $(this.options.test_el);
+            this.options.test_url = this.test_el.getElement('a').get('href');
+            this.test_el.addEvent('click', function(e) {
+                e.preventDefault();
+                controller.testAddon();
+            });
+
+            this.download_el = $(this.options.download_el);
+            this.options.download_url = this.download_el.getElement('a').get('href');
+            this.download_el.addEvent('click', function(e) {
+                e.preventDefault();
+                controller.downloadAddon();
+            });
+        }
+        this.copy_el = $(this.options.copy_el)
+        if (this.copy_el) {
+            this.copy_el.addEvent('click', function(e) {
+                e.preventDefault();
+                controller.copyPackage();
+            });
+        }
         if (this.options.check_if_latest) {
-            window.addEvent('focus', function() {
-                this.checkIfLatest(this.askForReload.bind(this));
-            }.bind(this));
+            dom.window.addEvent('focus', function() {
+                controller.checkIfLatest(this.askForReload);
+            });
         }
 
-		this.setupButtonTooltips();
+        this.packageInfoEl = $(this.options.package_info_el);
+        this.packageInfoEl.addEvent('click', function(e) {
+            e.preventDefault();
+            controller.showInfo();
+        });
+
+        this.setupButtonTooltips();
     },
 
     instantiate_modules: function() {
-		// iterate by modules and instantiate Module
-		this.options.modules.forEach(function(module) {
-			module.readonly = this.options.readonly;
-			module.append = true;
-			this.modules[module.filename] = new Module(this,module);
-		}, this);
-	},
+        // iterate by modules and instantiate Module
+        this.options.modules.forEach(function(module) {
+            module.readonly = this.options.readonly;
+            module.append = true;
+            this.modules[module.filename] = new Module(this,module);
+        }, this);
+    },
 
-	instantiate_attachments: function() {
-		// iterate through attachments
-		this.options.attachments.forEach(function(attachment) {
-			attachment.readonly = this.options.readonly;
-			attachment.append = true;
-			this.attachments[attachment.uid] = new Attachment(this,attachment);
-		}, this);
-	},
+    instantiate_attachments: function() {
+        // iterate through attachments
+        this.options.attachments.forEach(function(attachment) {
+            attachment.readonly = this.options.readonly;
+            attachment.append = true;
+            this.attachments[attachment.uid] = new Attachment(this,attachment);
+        }, this);
+    },
 
-	instantiate_dependencies: function() {
-		// iterate through attachments
-		this.options.dependencies.forEach(function(plugin) {
-			plugin.readonly = this.options.readonly;
-			plugin.append = true;
-			this.libraries[plugin.id_number] = new Library(this,plugin);
-		}, this);
-	},
-	
-	instantiate_folders: function() {
-		this.options.folders.forEach(function(folder) {
-			folder.append = true;
-			this.folders[folder.root_dir + '/' + folder.name] = new Folder(this, folder);
-		}, this);
-	},
+    instantiate_dependencies: function() {
+        // iterate through attachments
+        this.options.dependencies.forEach(function(plugin) {
+            plugin.readonly = this.options.readonly;
+            plugin.append = true;
+            this.libraries[plugin.id_number] = new Library(this,plugin);
+        }, this);
+    },
+    
+    instantiate_folders: function() {
+        this.options.folders.forEach(function(folder) {
+            folder.append = true;
+            this.folders[folder.root_dir + '/' + folder.name] = new Folder(this, folder);
+        }, this);
+    },
 
 
     showRevisionList: function() {
@@ -113,14 +142,15 @@ module.exports = new Class({
     },
 
     setupButtonTooltips: function() {
-        (typeof console !== 'undefined') && console.warn('no Button Tooltips!');
-        return false;
+        if(typeof FloatingTips === 'undefined') {
+            return false;
+        }
 
-		this.tips = new FloatingTips('.UI_Editor_Menu .UI_Editor_Menu_Button', {
-			position: 'top',
-			balloon: true
-		});
-	},
+        this.tips = new FloatingTips('.UI_Editor_Menu .UI_Editor_Menu_Button', {
+            position: 'top',
+            balloon: true
+        });
+    },
 
     checkIfLatest: function(failCallback) {
         // we want to make a request each time, since the author could
@@ -133,10 +163,135 @@ module.exports = new Class({
             callback: function(r) {
                 r = r[0];
                 if (r.get('revision_number') > controller.package.get('revision_number')) {
-                    failCallback();
+                    failCallback.call(controller);
                 }
             }
         })
+    },
+
+    askForReload: function() {
+        fd.warning.alert(
+            'New revision detected', 
+            'There is a newer revision available. <a href="'+ 
+            this.options.latest_url +'">Click this link to go to it now.</a>'
+        );
+    },
+
+    /*
+     * Method: copyPackage
+     * create a new Package with the same name for the current user
+     */
+    copyPackage: function() {
+        if (!settings.user) {
+            fd.alertNotAuthenticated();
+            return;
+        }
+        
+        if (fd.edited) {
+            fd.error.alert("There are unsaved changes", 
+                    "To make a copy, please save your changes.");
+            return;
+        }
+        new Request.JSON({
+            url: this.options.copy_url,
+            useSpinner: true,
+            spinnerTarget: this.copy_el.getElement('a'),
+            spinnerOptions: {
+                img: {
+                    'class': 'spinner-img spinner-16'
+                },
+                maskBorder: false
+            },
+            onSuccess: function(response) {
+                window.location.href = response.view_url;
+            }
+        }).send();
+    },
+
+    downloadAddon: function(e) {
+        if (e) {
+          e.stop();
+        }
+        var el = $(this.options.download_el).getElement('a');
+
+        fd.tests[this.options.hashtag] = {
+            spinner: new Spinner(el, {
+                img: {
+                    'class': 'spinner-img spinner-16'
+                },
+                maskBorder: false
+            }).show()
+        };
+        data = {
+            hashtag: this.options.hashtag, 
+            filename: this.options.name
+        };
+        new Request.JSON({
+            url: this.options.download_url,
+            data: data,
+            onSuccess: fd.downloadXPI
+        }).send();
+    },
+
+    testAddon: function(){
+        var el;
+        if (fd.alertIfNoAddOn()) {
+            if (e) {
+                el = e.target;
+            } else {
+                el = $(this.options.test_el);
+            }
+            if (el.getParent('li').hasClass('pressed')) {
+                fd.uninstallXPI(el.get('data-jetpackid'));
+            } else {
+                this.installAddon();
+            }
+        } else {
+            fd.whenAddonInstalled(function() {
+                fd.message.alert(
+                    'Add-on Builder Helper',
+                    'Now that you have installed the Add-on Builder Helper, loading the add-on into your browser for testing...'
+                );
+                this.testAddon();
+            }.bind(this));
+        }
+    },
+
+    installAddon: function() {
+        if (this._test_request && this._test_request.isRunning()) {
+            $log('FD: DEBUG: Test request already running. Cancelling second attempt');
+            return;
+        }
+        
+        var spinner = new Spinner($(this.options.test_el).getElement('a'), {
+            img: {
+                'class': 'spinner-img spinner-16'
+            },
+            maskBorder: false
+        }).show()
+        fd.tests[this.options.hashtag] = {
+            spinner: spinner
+        };
+        var data = this.data || {};
+        data['hashtag'] = this.options.hashtag;
+        this._test_request = new Request.JSON({
+            url: this.options.test_url,
+            data: data,
+            onSuccess: fd.testXPI,
+            onFailure: function() {
+                spinner.destroy();
+            }
+        }).send();
+    },
+
+    generateHashtag: function() {
+        if (this.getOption('readonly')) return;
+        this.options.hashtag = fd.generateHashtag(this.options.id_number);
+    },
+
+    //Package.View
+    showInfo: function() {
+        fd.displayModal(this.options.package_info);
     }
 
 });
--- media/jetpack/js/editor/tests/controllers/package.js
+++ media/jetpack/js/editor/tests/controllers/package.js
@@ -1,6 +1,8 @@
 var dom = require('shipyard/dom'),
     Request = require('shipyard/http/Request'),
     typeOf = require('shipyard/utils/type').typeOf,
+    object = require('shipyard/utils/object'),
+    string = require('shipyard/utils/string'),
 
     Spy = require('testigo/lib/spy').Spy,
     mockXHR = require('shipyard/test/mockXHR'),
@@ -9,13 +11,38 @@ var dom = require('shipyard/dom'),
     PackageRevision = require('../../models/PackageRevision'),
     PackageController = require('../../controllers/PackageController');
 
+
+var BUTTONS = {
+    'package-properties': string.uniqueID(),
+    'package-copy': string.uniqueID(), 
+    'try_in_browser': string.uniqueID(), 
+    'download': string.uniqueID(),
+    'error-console': string.uniqueID(),
+    'package-save': string.uniqueID()
+}
+
 function resetDom() {
     dom.$$('body *').dispose();
+
+    dom.window.$events = {};
 
     var body = dom.document.body;
 
     body.grab(new dom.Element('input', { id: 'version_name' }));
     body.grab(new dom.Element('div', { id: 'revisions_list' }));
+    
+    object.forEach(BUTTONS, function(href, id) {
+        var a = new dom.Element('a', { href: href });
+        var li = new dom.Element('li', { id: id });
+        li.grab(a);
+        body.grab(li);
+    });
+    
+}
+
+function E() {
+    this.preventDefault = new Spy;
+    this.stopPropagation = new Spy;
 }
 
 
@@ -23,42 +50,43 @@ function resetDom() {
 module.exports = {
     'PackageController': function(it, setup) {
 
-        var pack;
+        var addon;
 
         setup('beforeEach', function() {
             resetDom();
-            pack = new Package({
+            addon = new Package({
                 full_name: 'foo bar',
                 version_name: '0.5',
+                type: 'a',
                 revision_number: 2
             });
         });
 
         it('should instantiate', function(expect) {
             
-            var pc = new PackageController(pack, {
+            var pc = new PackageController(addon, {
                 modules: [
                     {id: 1, filename: 'foo'},
                     {id: 2, filename: 'bar'}
                 ]
             });
             expect(pc).toBeAnInstanceOf(PackageController);
-            expect(pc.package).toBe(pack);
+            expect(pc.package).toBe(addon);
         });
 
         it('should bind the package version_name', function(expect) {
-            var pc = new PackageController(pack, {});
+            var pc = new PackageController(addon, {});
 
-            expect(pc.versionEl.get('value')).toBe(pack.get('version_name'));
+            expect(pc.versionEl.get('value')).toBe(addon.get('version_name'));
 
             var newVer = '0.5.1';
-            pack.set('version_name', newVer);
+            addon.set('version_name', newVer);
 
             expect(pc.versionEl.get('value')).toBe(newVer);
         });
 
         it('should register revisions_list click', function(expect) {
-            var pc = new PackageController(pack, {});
+            var pc = new PackageController(addon, {});
 
             pc.showRevisionList = new Spy;
 
@@ -71,14 +99,69 @@ module.exports = {
         });*/
 
         it('should be able to determine if latest revision', function(expect) {
-            var pc = new PackageController(pack);
+            var pc = new PackageController(addon);
 
             mockXHR({ id: 1, revision_number: 3 });
 
             var failCallback = new Spy;
             pc.checkIfLatest(failCallback);
 
+            mockXHR({ id: 2, revision_number: 1 });
+            pc.checkIfLatest(failCallback);
+
             expect(failCallback.getCallCount()).toBe(1);
+        });
+        
+        it('should get the test_url from the dom', function(expect) {
+            var pc = new PackageController(addon);
+            expect(pc.getOption('test_url')).toBe(BUTTONS['try_in_browser'])
+        });
+
+        it('should be bound to testAddon', function(expect) {
+            var pc = new PackageController(addon);
+            pc.testAddon = new Spy;
+            pc.test_el.fireEvent('click', new E);
+            expect(pc.testAddon.getCallCount()).toBe(1);
+        });
+
+        it('should get the download_url from the dom', function(expect) {
+            var pc = new PackageController(addon);
+            expect(pc.getOption('download_url')).toBe(BUTTONS['download']);
+        });
+
+        it('should be bound to downloadAddon', function(expect) {
+            var pc = new PackageController(addon);
+            pc.downloadAddon = new Spy;
+            pc.download_el.fireEvent('click', new E);
+            expect(pc.downloadAddon.getCallCount()).toBe(1);
+        });
+
+        it('should be bound to copyPackage', function(expect) {
+            var pc = new PackageController(addon);
+            pc.copyPackage = new Spy;
+            pc.copy_el.fireEvent('click', new E);
+            expect(pc.copyPackage.getCallCount()).toBe(1);
+        });
+
+        it('should be bound to checkIfLatest on window.focus', function(expect) {
+            var pc = new PackageController(addon);
+            pc.checkIfLatest = new Spy;
+            dom.window.fireEvent('focus', new E);
+            expect(pc.checkIfLatest.getCallCount()).toBe(1);
+        });
+
+        it('should not be bound to checkIfLatest when viewing versions', function(expect) {
+            var pc = new PackageController(addon, { check_if_latest: false });
+            pc.checkIfLatest = new Spy;
+            dom.window.fireEvent('focus', new E);
+            expect(pc.checkIfLatest.getCallCount()).toBe(0);
+        });
+
+        it('should be bound to showInfo', function(expect) {
+            var pc = new PackageController(addon);
+            pc.showInfo = new Spy;
+            pc.packageInfoEl.fireEvent('click', new E);
+            expect(pc.showInfo.getCallCount()).toBe(1);
         });
     }
 }
