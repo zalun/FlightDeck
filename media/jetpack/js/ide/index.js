f85e0aa5f673829544f72cb68b0cddaa2e9e6b43
Sean McArthur <sean.monstar@gmail.com>
2011-12-29 12:54:36 -0800

    ide app now has an init function, instead of initting when you require('ide');

--- media/jetpack/js/ide/index.js
+++ media/jetpack/js/ide/index.js
@@ -9,22 +9,25 @@ var object = require('shipyard/utils/object');
 var log = require('shipyard/utils/log');
 require('./utils/request');
 
-// TODO: remove this once no files outside 'editor' app need the editor
-window.editor = exports;
+exports.init = function() {
+	log.debug('ide/index loaded');
 
+	var ide = {};
+	var tabs = ide.tabs = new TabsController();
+	var sidebar = ide.sidebar = new Sidebar({
+		editable: !settings.readonly
+	});
+	var ace = ide.ace = new Ace('editor-wrapper');
 
-log.debug('ide/index loaded');
+	var data = object.merge({}, settings, {
+		description: settings.package_description,
+		author: settings.package_author,
+		version_name: settings.package_version_name
+	});
+	var p = ide.item = new Package(data);
+	ide.controller = new PackageController(p, settings, ace, tabs, sidebar);
 
-var tabs = exports.tabs = new TabsController();
-var sidebar = exports.sidebar = new Sidebar({
-    editable: !settings.readonly
-});
-var ace = exports.ace = new Ace('editor-wrapper');
-
-var data = object.merge({}, settings, {
-    description: settings.package_description,
-    author: settings.package_author,
-    version_name: settings.package_version_name
-});
-var p = exports.item = new Package(data);
-exports.controller = new PackageController(p, settings, ace, tabs, sidebar);
+	// TODO: remove this once no files outside 'editor' app need the editor
+	window.editor = ide;
+	return ide;
+};
