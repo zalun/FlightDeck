4a9e13ad9181640a2bd4eccd7960ed0e8d21ce17
Arron Schaar <aschaar@gmail.com>
2012-02-15 11:18:33 -0800

    added semi-colons to each line

--- migrations/029-change_charset.sql
+++ migrations/029-change_charset.sql
@@ -1,4 +1,4 @@
-ALTER TABLE jetpack_module CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci
-ALTER TABLE jetpack_attachment CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci
-ALTER TABLE jetpack_package CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci
-ALTER TABLE jetpack_packagerevision CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE jetpack_module CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE jetpack_attachment CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE jetpack_package CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE jetpack_packagerevision CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
